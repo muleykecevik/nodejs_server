5d855d445d786fc56d83dcd0b9a0a389dcca4c8e
muleykecevik <muleyke.cevik@gmail.com>
2024-02-28 20:07:37 +0300

    28.04.2022

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ NodeJs SERVER
 //console.log("welcome");
 
 //server ve host no yu .env den alalim
-require('dotenv').config();
+require('dotenv').config(); // conmfig dedigimizde sadece configdeki dosyalari calistir deriz boylece oradaki verielr processs.env ye gelmistir // env dosyasindakileri almak icin env require dedik. npm i dotenv dedik
 const PORT = process.env?.PORT || 8000
 const HOST = process.env?.HOST || "127.0.0.1"
 const http = require("node:http")
-//http.createServer((param1,param2)=>{ // 1.par her zaman req 2. her zaman response
-//})
+
+//http.createServer((param1,param2)=>{ // 1.par her zaman req 2. her zaman response, bununla server olusturuyoruz
+//})  bunun yerine daha kolayi kullaniyoruz
 //listen(8000, () => console.log("server runned : http://127.0.0.1:8000"
 const app = http.createServer((req, res) => {
     /*
@@ -21,7 +22,13 @@ const app = http.createServer((req, res) => {
     console.log(req.url);
     */
     //end points home: /,list: /list:, test: /test
-    //    if(req.url=='/')
+    //    if(req.url=='/') {
+    //     res.end('<h1>HOME Page</h1>')
+    // }
+    //    else if(req.url=='/list'){
+    //     res.end('<h1>HOME Page</h1>')
+    // } 
+    //   else if(req.url=='/test'){
     //     res.end('<h1>HOME Page</h1>')
     // }
 
@@ -36,11 +43,21 @@ const app = http.createServer((req, res) => {
             res.write('page')
             res.end()
         }
+        else if(req.method=='POST'){
+            res.end(' can not use this method')
+        } else if (req.method=='DELETE'){
+            
 
-        else
-            res.end('cannot use this method')
+
+    } else if (req.url == '/list') {
+        res.end(' cannot use this method')
+    } else if (req.url == '/test') {
+        res.end('<h1> TEST PAGE </>')
     }
 
-    app.listen(8000, () => console.log(`server runned : http://${HOST}:${PORT}`))
 
-})
+
+    // default local api si her zaman 127.0.0.1 dir. listen diyerek bir portu dinle diyoruz. nodejs de genelde 8000 kullanilir. bosta baska port varsa oda kullanilabilir. 
+
+
+}).listen(8000, () => console.log(`server runned : http://${HOST}:${PORT}`))
